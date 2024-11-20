9ac9eac2bc457aa93f1ac813ae0e0c01d963ed26
luis alcivar <alcivarluis250@gmail.com>
2024-11-20 09:47:36 -0500

    hola mundo

---
+++ ejemplo1.cpp
@@ -0,0 +1,14 @@
+//programa 1 estructura de c++
+#include <stdio.h>
+#include <iostream>
+int main(){
+    /*
+       Mi primer programa usando los lenguajes c y c++
+       realizando en el laboratorio g202
+       autor Luis Alcivar
+    */
+    printf("hola mundo\n");
+    std::cout<<"estamos en c++"<<std::endl;
+    std::cout<<"c++ esta bonito";
+}
+
