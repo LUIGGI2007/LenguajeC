7f202d1ee69e7517ff783f3bc52dd5b69b6d5e97
josselynandrea <jaulcuango1@espe.edu.ec>
2025-01-17 10:55:38 -0500

    bidimensional 17/01/2025

---
+++ arrays/bidimensional/definicion.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+main()
+{
+    int tabña[3][3];
+    float pesos[2][3]={{1.2,3.4,5.6},{7.8,8.0,1.2}};//declaramiento e inicializacion
+    cout <<pesos[0][0]<<endl;//imprimiendo el primer elemento de la segunda fila y primera columna
+    cout<<pesos[1][1]<<endl;
+}
